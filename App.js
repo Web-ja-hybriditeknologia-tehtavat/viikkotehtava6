e5eb3caa2e1e0f4701052dbe92d57c976ef422f8
Ronja Ahonen <ronja.e.a.ahonen@gmail.com>
2025-02-19 21:19:31 +0200

    valmis

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+
+
+const URL = 'https://dog.ceo/api/breeds/image/random'
+
+
 
 export default function App() {
+
+  const [Dogge, setDog] = useState('')
+
+  const fetchDogImage = () => {
+    fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+      setDog(data.message)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  useEffect(() => {
+    fetchDogImage();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Here is a dog:</Text>
+      <Image 
+      style={styles.tinyLogo}
+      source={{uri: Dogge }}/>
+
+      <Button
+        onPress={fetchDogImage}
+        title="Reload"
+        color="#841584"
+      />
     </View>
   );
 }
@@ -16,5 +46,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  tinyLogo: {
+    width:200,
+    height:200,
+    margin:5,
   },
 });
